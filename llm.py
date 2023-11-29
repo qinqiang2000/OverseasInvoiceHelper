70e0518cb431725377ecc5fb137e6922a8ddd0d1
qinqiang2000 <qinqiang2000@qq.com>
2023-11-29 15:41:44 +0800

    3.12.1 继续优化prompt

--- llm.py
+++ llm.py
@@ -58,17 +58,19 @@ template = """
 -发票日期（Invoice Date）。
 -币种（Currency）: 应明确标出，例如 'USD'。
 -总金额（Amount）: 以数字类型提取，如果没有"Amount"，则找"Total Amount"或"Total Value"或其他类似的词语，找不到置为0。
--收票方（Bill To）: 如果没有"Bill To"，则找'MESSRS'、'Purchaser' 、'Customer'或其他和'购买方'同义的词语；大小写不敏感。如果"Bill To" 包含'LOGISTICS'、'Express worldwide' 或类似'物流公司'的信息，将"Bill To"和"Ship To"的值调换。
+-收票方（Bill To）: 如果没有"Bill To"，则找'MESSRS'、'Purchaser' 、'Customer'或其他和'购买方'同义的词语；大小写不敏感。
 -开票方（From）: 如果没有"From" 信息, 或者不像一个公司名称，则找：'Account Name'、'Beneficiary Name'、底部签名处或标题; 大小写不敏感。
 -Ship To
 
 注意：
+-识别出常见收票方和开票方的公司名称组成部分，如“Group”, “Corporation”, “Limited”, “Inc.”等。
 -识别并分割紧凑排列的单词，尤其是公司名称，如“WaterWorldInternationalIndustrialLimited”，正确分词为“Water World International Industrial Limited”。
 -确保日期信息有正确空格。
 -如果找不到对应信息，则json的值置为空。
 -"Bill To" 或 "From" 如果有地址信息，删除它们。
 -"Bill To" 或 "From" 如果没正确分词，对他们进行分词; 如果有中文，直接提取，不要翻译。
--OCR的内容可能存在名词被切断、个别字母识别错误、对应错位等问题，你需要结合上下文语义进行综合判断，以抽取准确的关键信息。
+-OCR的内容可能存在名词被切断、个别字母识别错误、对应错位等问题，你需要结合上下文语义进行综合判断，以抽取准确的关键信息。比如“Packing List”被识别成" Packihg
+List"或"PACKINGLIST"
 -仅输出JSON结果，不包含其他文字。
 """
 
